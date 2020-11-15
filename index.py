4fce67ce2d147168b2003ea381a72450e11af753
toily.code <escusado@gmail.com>
2020-11-14 19:33:29 -0600

    Update index.py

--- index.py
+++ index.py
@@ -12,8 +12,10 @@ now = 0
 
 def short_click():
     episode = random.choice(os.listdir(directory)).replace('.mp4', '').replace(
-        '.mkv', '')
-    print((Figlet(font='slant')).renderText(episode))
+        '.mkv', '').split(' - ')
+
+    print((Figlet(font='slant')).renderText(episode[0]))
+    print((Figlet(font='slant')).renderText(episode[1]))
 
     print("SHORT", episode)
 

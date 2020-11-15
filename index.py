07816eca64f9bbc7b6f07e3adc25f11666f3b247
toily.code <escusado@gmail.com>
2020-11-14 20:12:39 -0600

    Update index.py

--- index.py
+++ index.py
@@ -80,8 +80,7 @@ GPIO.setmode(GPIO.BOARD)
 # Set pin 10 to be an input pin and set initial value to be pulled low (off)
 GPIO.setup(10, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 # Setup event on pin 10 rising edge
-GPIO.add_event_detect(10, GPIO.RISING, callback=button_callback)
-# Run until someone presses enter
-message = input("Press enter to quit\n\n")
-# Clean up
-GPIO.cleanup()
+GPIO.add_event_detect(10, GPIO.BOTH, callback=button_callback)
+
+while True:
+    print('foo', GPIO.input(10))
